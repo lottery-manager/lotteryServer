fe2d854a8f625d7a9a0aae43df5a586d70b6cb0a
MrMessy77 <313691730@qq.com>
2018-03-20 17:10:05 +0800

    添加config

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports= {
+    webServer: {
+        "listenIp": "0.0.0.0",
+        "port": "8080",
+        "secret": "lottery",
+        cookieTime: 1000*60*60*24*10
+    },
+    mongoDB :{
+        dbIp : "www.jsstack.cn",
+        dbPort : "27017",
+        dbName : "lottery",
+        user : "lottery",
+        pass : "lottery"
+    }
+};
